7edd92862581bc444b67d75e8cb2fe507d91fb05
Hussein Work <hus.sc@aol.com>
2021-01-15 09:35:23 +0200

    Initial Commit

---
+++ qurani-core/qurani_core.h
@@ -0,0 +1,18 @@
+//
+//  qurani_core.h
+//  qurani-core
+//
+//  Created by Hussein Work on 15/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for qurani_core.
+FOUNDATION_EXPORT double qurani_coreVersionNumber;
+
+//! Project version string for qurani_core.
+FOUNDATION_EXPORT const unsigned char qurani_coreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <qurani_core/PublicHeader.h>
+
+
